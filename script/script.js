dd27a1e71ec12012654da22738784757f58f6136
PipouilleJohnson <killian.bouzera77150@gmail.com>
2023-09-17 15:04:41 +0200

    save 17092023 15h04

---
+++ script/script.js
@@ -0,0 +1,12 @@
+const aPropos = document.getElementById("apropos");
+const cv = document.getElementById("apropos");
+const echantillons = document.getElementById("apropos");
+const portfolio = document.getElementById("apropos");
+const contact = document.getElementById("apropos");
+
+
+aPropos.addEventListener("click", ()=>{
+    console.log("hello !");
+    aPropos.style.textDecoration = "underline";
+    cv.style.textDecoration = "none";
+})
